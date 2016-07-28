061c8464645d31a960b6e542bb57c8fc19ef6f32
Oleg Chupyra <ovchupyra@gmail.com>
2016-07-28 23:30:10 +0300

    First commit maven

--- src/ru/gum/ats/logzocfx/tool/AddNull.java
+++
@@ -1,8 +0,0 @@
-package ru.gum.ats.logzocfx.tool;
-
-/**
- * Created by chupiraov on 16.05.2016.
- */
-public interface AddNull {
-    String addNull(Integer number, int size);
-}
--- src/ru/gum/ats/logzocfx/tool/AddingChar.java
+++
@@ -1,8 +0,0 @@
-package ru.gum.ats.logzocfx.tool;
-
-/**
- * Created by chupiraov on 13.05.2016.
- */
-public interface AddingChar {
-    String addChar(int k, char ch, String str);
-}
